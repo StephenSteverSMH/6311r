44c1c7b1d9ed4dcd53d396016fbd51f6d8daca25
Best aa <1094662978@qq.com>
2021-06-10 00:54:27 +0800

    m2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:6331server1.database.windows.net,1433;Database=database1;Uid=Stephen6331;Pwd=Password6331;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-    conn = pymssql.connect(server='6331server1.database.windows.net', port=1433, user='Stephen6331', password='Password6331', database='database1')
+    conns = pymssql.connect(server='6331server1.database.windows.net', port=1433, user='Stephen6331', password='Password6331', database='database1')
     return 'hello world!'
 
 if __name__ == '__main__':
